a894a8b82877c10746ca5d95a41f2ac88abac32f
Parsa Poorsistani <poorsistani13@gmail.com>
2023-07-02 01:51:09 +0330

    removed the clap and use std::env

--- src/cli.rs
+++ src/cli.rs
@@ -1,69 +1,56 @@
-use clap::{App, Arg};
+use std::env;
 
-
-pub struct Arguments {
-    pub patterns: Vec<String>,
+// thread should be added
+pub struct Argument {
+    pub pattern: String,
     pub files: Vec<String>,
     pub ln: bool,
     pub depth: Option<usize>,
-    pub nthreads: usize,
     pub invert_match: bool,
 } 
 
-pub fn parse_argument() -> Arguments {
-    let matches = App::new("grep")
-                    .about("a grep clone written in Rust")
-                    .arg(
-                        Arg::new("patterns")
-                            .multiple(true)
-                            .required(true)
-                            .about("the patterns to search for"),
-                    )
-                    .arg(
-                        Arg::new("file")
-                            .multiple(true)
-                            .about("The files to search within"),
-                    )
-                    .arg(
-                        Arg::new("line-number")
-                            .short("ln")
-                            .long("line-number")
-                            .about("show line numbers of the matched lines"),
-                    )
-                    .arg(
-                        Arg::new("depth")
-                            .short('d')
-                            .long("depth")
-                            .takes_value(true)
-                            .about("Set the depth of recursive directory search"),
-                    )
-                    .arg(
-                        Arg::new("threads")
-                            .short('t')
-                            .long("threads")
-                            .takes_value(true)
-                            .about("Set the number of threads to use for search"),
-                    )
-                    .arg(
-                        Arg::new("invert-match")
-                            .short('v')
-                            .long("invert-match")
-                            .about("Select non-matching lines"),
-                    )
-                    .get_matches();
-    let patters = matches.get_many("patterns").unwrap_or_default();
-    let files = matches.get_many("files").unwrap_or_default();
-    let line_number = matches.is_present("line-number");
-    let depth = matches.get_one("depth").map(|val| val.parse().unwrap());
-    let nthreads = matches.get_one("threads").map(|val| val.parse().unwrap_or(1));
-    let invert_match = matches.is_present("invert-match");
+pub fn parse_argument() -> Argument{
+    let args:Vec<String> = env::args().collect();
+    println!("first {}",args[1]);
+    let mut pattern = String::new();
+    let mut files:Vec<String> = Vec::new();
+    let mut ln = false;
+    let mut depth:Option<usize> = None;
+    let mut invert_match = false;
 
-    Arguments {
-        patterns,
-        files,
-        line_number,
-        depth,
-        nthreads,
-        invert_match,
+    let mut i = 1;
+    while i<args.len() {
+        match args[i].as_str() {
+            "-ln" => {
+                ln = true;
+            }
+
+            "-d" => {
+                i+=1;
+                depth = args[i].parse::<usize>().ok();
+            }
+
+            "-i" => {
+                invert_match = true;
+            }
+
+            arg => {
+                if pattern.is_empty() {
+                    pattern = arg.to_owned();
+                } else {
+                    files.push(arg.to_owned());
+                }
+            }
+        }
+        i+=1;
     }
+
+    Argument {
+        pattern,
+        files,
+        ln,
+        depth,
+        invert_match
+    }
+
 }
